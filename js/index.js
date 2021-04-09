d15fd148d05423ecb65eb258d55e588563aeacda
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-04-09 15:29:07 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// AOS
+AOS.init();
+
+// Navigation
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const navContainer = document.querySelector(".nav__menu");
+
+navOpen.addEventListener("click", () => {
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
+});
+
+navClose.addEventListener("click", () => {
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
+});
